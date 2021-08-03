b22cee1d0c7d8fe70f67836b93a1edc9d890e076
ANarayan <gostanfordtennis@gmail.com>
2021-08-03 13:53:26 -0700

    minor mods go global paths

--- globals.py
+++ globals.py
@@ -2,10 +2,8 @@ import os
 
 PATH_HERE = os.path.abspath(os.path.dirname(__file__))
 MODEL_CONFIG_DIR = os.path.join(PATH_HERE, "model-configs")
-# EXPERIMENT_CONFIGS_DIR = '/experiments/ludwig-bench-textclassification/experiment-configs'
 EXPERIMENT_CONFIGS_DIR = os.path.join(PATH_HERE, "hyperopt-experiment-configs")
-#DATASET_CACHE_DIR = os.path.join(PATH_HERE, "datasets")
-DATASET_CACHE_DIR = "s3://experiments.us-west-2.predibase.com/tabular-experiments/test-datasets"
+DATASET_CACHE_DIR = "s3://experiments.us-west-2.predibase.com/tabular-experiments-small/datasets/"
 ENERGY_LOGGING_DIR = os.path.join(PATH_HERE, "energy_logging")
 
 MODEL_HYPEROPT_FILENAMES = {
@@ -29,7 +27,7 @@ DATASETS_LIST = ["RossmannStoreSales"]
 CONFIG_TEMPLATE_FILE = "./experiment-templates/task_template.yaml"
 DATASET_METADATA_FILE = "./experiment-templates/dataset_metadata.yaml"
 HYPEROPT_CONFIG_FILE = "./experiment-templates/hyperopt_config.yaml"
-EXPERIMENT_OUTPUT_DIR = "s3://experiments.us-west-2.predibase.com/tabular-experiments/test-exp-2"
+EXPERIMENT_OUTPUT_DIR = "s3://experiments.us-west-2.predibase.com/tabular-experiments-small/results/"
 
 PATH_TO_PRETRAINED_EMBEDDINGS = None
 
